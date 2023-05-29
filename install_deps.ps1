d5d9c7af87f0055a1eaec4d34e86b985cb0087ee
Prikryl <uidn4021@contiwan.com>
2023-05-29 14:00:22 +0200

    first commit

---
+++ install_deps.ps1
@@ -0,0 +1,7 @@
+# run this in terminal first: <Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process>, then run this script in the same terminal (PS)
+python -m venv .venv_python_tests
+.venv_python_tests\Scripts\activate.ps1
+cd .venv_python_tests\Scripts
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+cd ../..
